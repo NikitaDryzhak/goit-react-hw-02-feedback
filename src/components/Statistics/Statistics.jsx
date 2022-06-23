9fd6358bb75190afcef048eac72c31e5352214f0
Nikita Dryzhak <87204570+NikitaDryzhak@users.noreply.github.com>
2022-06-23 15:57:26 +0300

    props

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -10,7 +10,7 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage}) =>
             <p>Neutral: {neutral}</p>
             <p>Bad: {bad}</p>
             <p>Total: {total}</p>
-            <p>Positive feedback: {positivePercentage}%</p>
+            <p>Positive feedback: {positivePercentage} %</p>
         </>
     )
 }
@@ -24,5 +24,5 @@ Statistics.propTypes = {
     neutral: PropTypes.number.isRequired,
     bad: PropTypes.number.isRequired,
     total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.string.isRequired,
 }
