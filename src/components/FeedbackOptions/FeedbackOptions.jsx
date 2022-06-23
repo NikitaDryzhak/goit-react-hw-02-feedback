f6fad735db980ee6ec96d53ead7060a14b75959a
Nikita Dryzhak <87204570+NikitaDryzhak@users.noreply.github.com>
2022-06-23 15:12:35 +0300

    add src

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,20 @@
+import './FeedbackOptions.css'
+
+const FeedbackOptions = ({options, onLeaveFeedback}) => {
+    return (
+        <>
+        <ul>
+        {options.map(option => (
+             <li key={option}>
+                <button name={option} onClick={onLeaveFeedback}>{option}</button>
+            </li>   
+            ))}
+        </ul>
+        </>
+    
+)
+    
+    
+}
+
+export default FeedbackOptions
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,12 @@
+import './Section.css'
+
+const Section = ({ title, children }) => {
+    return (
+        <>
+            <h1>{title}</h1>
+            {children}
+        </>
+    )
+}
+
+export default Section
